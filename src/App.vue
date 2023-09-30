6343fc30baf29c492885df9b9d03f41b089764c9
sehwan <nu940907@gmail.com>
2023-09-30 12:37:29 +0900

    component, plugin 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Chart.js</h1>
+    <bar-chart></bar-chart>
+    <line-chart :line="lineData"></line-chart>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BarChart from './components/BarChart.vue'
+import LineChart from './components/LineChart.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: { BarChart, LineChart },
+  data() {
+    return {
+      lineData: [{
+        label: "My First dataset",
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+      }],
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,30 @@
+<template>
+    <canvas ref="lineChart" id="lineChart"></canvas>
+</template>
+
+<script>
+export default {
+    props: ['line'],
+    mounted() {
+        console.log(this.line);
+        var ctx = this.$refs.lineChart.getContext('2d');
+        new this.$_Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+
+            // The data for our dataset
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: this.line
+            },
+
+            // Configuration options go here
+            options: {}
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
